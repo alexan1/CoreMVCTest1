cab5095b99823f0b96ef65ff60a3f009ebf1643e
Alexandre Konioukhov <alexan1@gmail.com>
2019-06-10 14:46:33 -0400

    layout

--- CoreMVCTest1/Startup.cs
+++ CoreMVCTest1/Startup.cs
@@ -73,7 +73,7 @@ namespace CoreMVCTest1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=TestEntities}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
